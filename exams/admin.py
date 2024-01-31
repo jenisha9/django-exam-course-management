c141ad74a48259f727c4043cc41a15687eb36f97
jenisha9 <njenisha78@gmail.com>
2024-01-31 12:39:01 +0545

    feat(new model): add new model for College

--- exams/admin.py
+++ exams/admin.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 
-from .models import Course, EntranceExam
+from .models import College, Course, EntranceExam
 
 admin.site.register(EntranceExam)
 admin.site.register(Course)
+admin.site.register(College)
 
--- exams/models.py
+++ exams/models.py
@@ -5,15 +5,28 @@ class EntranceExam(models.Model):
     name = models.CharField(max_length=255)
     full_mark = models.IntegerField(default=100)
     pass_mark = models.IntegerField()
+
     def __str__(self):
         return self.name
 
+class College(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
     eligibility_exam = models.ForeignKey(
-        EntranceExam, default=None, on_delete=models.CASCADE
+        EntranceExam, on_delete=models.CASCADE
+    )
+    offering_college = models.ManyToManyField(
+        College
     )
 
     def __str__(self):
         return self.name
+
+
+
