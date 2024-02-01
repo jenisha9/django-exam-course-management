fde636d033b1e298cd8fff7f8635b7486ad8309a
jenisha9 <njenisha78@gmail.com>
2024-02-01 12:09:35 +0545

    feat(field): add new fields in model EntranceExam

--- exams/admin.py
+++ exams/admin.py
@@ -7,3 +7,10 @@ admin.site.register(EntranceExam)
 admin.site.register(Course)
 admin.site.register(College)
 
+class EntranceExamAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['exam_name', 'university','exam_date','slug']}),
+    ]
+    prepopulated_fields = {
+        "slug": ("exam_name",),
+    }
