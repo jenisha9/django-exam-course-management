48f9ad20083c453044f898601165ecf21a4f24f9
jenisha9 <njenisha78@gmail.com>
2024-02-05 14:53:07 +0545

    feat(api): refactor view with wrapper API view

--- exams/views.py
+++ exams/views.py
@@ -1,42 +1,44 @@
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import EntranceExam
 from .serializer import EntranceExamSerializer
 
 
-def exam_list(request):
-    if request.method == 'GET':
-        exam = EntranceExam.objects.all()
-        serializer = EntranceExamSerializer(exam, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = EntranceExamSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    
+@api_view(["GET", "PUT", "DELETE"])
 def exam_detail(request, pk):
     try:
         exam = EntranceExam.objects.get(pk=pk)
     except EntranceExam.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    if request.method == "GET":
         serializer = EntranceExamSerializer(exam)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = EntranceExamSerializer(exam, data=data)
+    elif request.method == "PUT":
+        serializer = EntranceExamSerializer(exam, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         exam.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET", "POST"])
+def exam_list(request):
+    if request.method == "GET":
+        exam = EntranceExam.objects.all()
+        serializer = EntranceExamSerializer(exam, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = EntranceExamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
