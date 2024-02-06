82fa07632675bd2daeb53cf966ac12dd880bb87b
jenisha9 <njenisha78@gmail.com>
2024-02-06 18:07:00 +0545

    feat: filter and order fuctionality

--- exams/views.py
+++ exams/views.py
@@ -1,14 +1,17 @@
+from rest_framework import filters, generics
+
 from .models import EntranceExam
 from .serializer import EntranceExamSerializer
-
-from rest_framework import generics
 
 
 class ExamList(generics.ListCreateAPIView):
     queryset = EntranceExam.objects.all()
     serializer_class = EntranceExamSerializer
-
-
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description', 'university']
+    ordering_fields = ['name', 'full_mark', 'pass_mark']
+     
 class ExamDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = EntranceExam.objects.all()
     serializer_class = EntranceExamSerializer
+    
